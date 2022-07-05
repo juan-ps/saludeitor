c82229a592c8632b75dac3259c74a3b7ccae9924
juan-ps <juanps.rt@gmail.com>
2022-07-05 14:39:34 -0300

    saludeitor

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Programa que saluda al usuario a través de la consola las veces que quiera
-
+alert("Bienvenid@ a Saludeitor! El mejor saludador por consola de Internet")
+alert("Siga las instrucciones a continuación para recibir nuestros saludos")
 let multiplo = parseInt(prompt("Ingrese la cantidad de saludos que quiera recibir"));
 let nombre = prompt("Ingrese su nombre a continuación");
 alert("A continuacion le pedimos que abra la consola con F12 para ver nuestros saludos :)");
